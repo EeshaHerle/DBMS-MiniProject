9bd9966c1169df5f72661962fb7a75c0050c007a
EeshaHerle <eeshaherle@gmail.com>
2023-06-21 18:04:13 +0530

    first commit

---
+++ home/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 10%;
+    height: 10%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #2ab7ca;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px"></p><br>
+  <a href="mailto:eeshaherle@gmail.com" style="color:white;">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
